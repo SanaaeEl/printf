b9d8d5d18fa0e22cf344fe9c1ca2853bd0b3e6d4
SanaaeEl <assanaae@gmail.com>
2024-09-04 08:22:47 +0100

    initial _printf commit

---
+++ _printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string containing specifiers
+ * Return: the nb of characters printed
+ */
+
+
+
+
+int _printf(const char *format, ...)
+{
+	ssize_t nb;
+
+	nb = write(STOUT_FILENO, format, strlen(format));
+	if (nb == -1)
+		return (-1);
+	return (nb);
+}
